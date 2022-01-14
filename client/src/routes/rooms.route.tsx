e87191fd2e58593aa69a7c3f863be45fc5f5b25a
checker <trinhlehainam@protonmail.com>
2022-01-14 18:40:21 +0900

    Fixed svg layout

--- client/src/routes/rooms.route.tsx
+++ client/src/routes/rooms.route.tsx
@@ -41,104 +41,109 @@ const Rooms = () => {
         <div
             className={cx(
                 "absolute inset-0 sm:inset-6 bg-black/10 backdrop-blur-xl",
-                "flex items-center justify-center"
+                "flex items-center justify-center",
+                "text-yellow-custom"
             )}
         >
             <div
                 className={cx(
-                    "w-full h-full p-4",
-                    "relative flex items-center justify-center"
+                    "w-full h-full p-4 max-w-[1280px] ",
+                    "relative flex flex-col items-center"
                 )}
             >
-                <RoomBorder classname="w-[90%] h-full max-w-[1280px]" />
-                <div
-                    className={cx(
-                        "absolute top-[7%]",
-                        "flex flex-col items-center",
-                        "text-yellow-custom text-3xl",
-                        "md:text-5xl"
-                    )}
-                >
-                    <RoomTitleBorderTop classname="w-[80%] sm:w-fit h-auto" />
-                    <div className="my-2">ROOMS</div>
-                    <RoomTitleBorderBottom classname="w-[80%] sm:w-fit h-auto" />
-                </div>
-                <RoomCancelButton
-                    classname={cx(
-                        "absolute top-[82%] left-[50%] transform -translate-x-1/2 sm:top-0 sm:right-0 w-[64px] h-auto sm:hidden"
-                    )}
-                    path="/play"
-                />
-                <RoomLine1 classname="w-[75%] max-w-[1107px] h-auto max-h-6 absolute top-[32%] sm:top-[30%]" />
-                <RoomLine2 classname="w-[75%] max-w-[1107px] h-auto max-h-4 absolute top-[40%]" />
-                <RoomLine3 classname="w-[75%] max-w-[1107px] h-auto max-h-12 absolute top-[70%]" />
-                <div
-                    className={cx(
-                        "absolute top-[34.3%] w-[75%] max-w-[1107px] max-h-[35.2%] overflow-hidden",
-                        "flex flex-col items-center",
-                        "text-yellow-custom"
-                    )}
-                >
-                    <div className="flex flex-row w-full text-lg sm:text-3xl justify-center items-center mb-8 sm:mb-12 text-center ">
+                <RoomBorder classname="w-[90%] h-full mx-auto" />
+                <div className="absolute flex flex-col items-center w-full h-full">
+                    <div
+                        className={cx(
+                            "flex flex-col items-center",
+                            "mt-16",
+                            "text-yellow-custom text-3xl",
+                            "md:text-5xl"
+                        )}
+                    >
+                        <RoomTitleBorderTop classname="w-[80%] sm:w-fit h-auto" />
+                        <div className="my-2">ROOMS</div>
+                        <RoomTitleBorderBottom classname="w-[80%] sm:w-fit h-auto" />
+                    </div>
+                    <RoomLine1 classname="w-[75%] h-auto max-h-6 mt-8 mx-auto" />
+                    <div className={cx(
+                        "flex flex-row justify-center items-center",
+                        "w-[75%] my-4 mx-auto",
+                        "text-lg sm:text-3xl text-center"
+                    )}>
                         <div className="w-1/3">NAME</div>
                         <div className="w-1/3">PLAYERS</div>
                         <div className="w-1/3"></div>
                     </div>
-                    <div className="flex flex-col h-full w-full overflow-y-auto scrollbar-hide gap-[5px]">
-                        {avaiRooms &&
-                            avaiRooms.map(({ roomId, clients, maxClients }, idx) => {
-                                return (
-                                    <AvailableRoom
-                                        key={`${roomId}-${clients}/${maxClients}`}
-                                        name={roomId}
-                                        players={`${clients}/${maxClients}`}
-                                    />
-                                );
-                            })}
-                    </div>
-                    <div></div>
-                </div>
-                <div
-                    className={cx(
-                        "absolute top-[75%] sm:top-[79%] w-3/4",
-                        "flex justify-center items-center -space-x-1 sm:gap-x-8"
-                    )}
-                >
+                    <RoomLine2 classname="w-[75%] h-auto max-h-4 mx-auto" />
                     <div
-                        className="relative flex justify-center items-center cursor-pointer"
-                        onClick={() => {
-                            client &&
-                                client.getAvailableRooms("MyRoom").then((rooms) => {
-                                    setAvaiRooms(rooms);
-                                });
-                        }}
+                        className={cx(
+                            "w-[75%] h-full max-h-[30.0%] overflow-hidden",
+                            "flex flex-col items-center mx-auto mt-4",
+                        )}
                     >
-                        <RoomButton classname="h-auto w-2/3 min-w-[80px] btn-base" />
-                        <div className="absolute text-yellow-custom text-md sm:text-2xl pointer-events-none">
-                            REFRESH
+                        <div className="flex flex-col h-full w-full overflow-y-auto scrollbar-hide gap-[5px]">
+                            {avaiRooms &&
+                                avaiRooms.map(({ roomId, clients, maxClients }, idx) => {
+                                    return (
+                                        <AvailableRoom
+                                            key={`${roomId}-${clients}/${maxClients}-${idx}`}
+                                            name={roomId}
+                                            players={`${clients}/${maxClients}`}
+                                        />
+                                    );
+                                })}
                         </div>
                     </div>
+                    <RoomLine3 classname="w-[75%] h-auto max-h-12 mx-auto mb-4" />
                     <div
-                        className="relative flex justify-center items-center cursor-pointer"
-                        onClick={() => {
-                            client && client.create('MyRoom')
-                                .then((room) => {
-                                    setRoom && setRoom(room);
-                                });
-                            navigate('/lobby');
-                        }}
+                        className={cx(
+                            "flex justify-center items-center -space-x-1 sm:gap-x-8",
+                            "w-3/4 mx-auto mb-16",
+                        )}
                     >
-                        <RoomButton classname="h-auto w-2/3 min-w-[80px] btn-base" />
-                        <div className="absolute text-yellow-custom text-md sm:text-2xl pointer-events-none">
-                            CREATE
+                        <div
+                            className="relative flex justify-center items-center cursor-pointer"
+                            onClick={() => {
+                                client &&
+                                    client.getAvailableRooms("MyRoom").then((rooms) => {
+                                        setAvaiRooms(rooms);
+                                    });
+                            }}
+                        >
+                            <RoomButton classname="h-auto w-2/3 min-w-[80px] btn-base" />
+                            <div className="absolute text-yellow-custom text-md sm:text-2xl pointer-events-none">
+                                REFRESH
+                            </div>
+                        </div>
+                        <div
+                            className="relative flex justify-center items-center cursor-pointer"
+                            onClick={() => {
+                                client && client.create('MyRoom')
+                                    .then((room) => {
+                                        setRoom && setRoom(room);
+                                    });
+                                navigate('/lobby');
+                            }}
+                        >
+                            <RoomButton classname="h-auto w-2/3 min-w-[80px] btn-base" />
+                            <div className="absolute text-yellow-custom text-md sm:text-2xl pointer-events-none">
+                                CREATE
+                            </div>
+                        </div>
+                        <div className="relative flex justify-center items-center cursor-pointer">
+                            <RoomButton classname="h-auto w-2/3 min-w-[80px] btn-base" />
+                            <div className="absolute text-yellow-custom text-md sm:text-2xl pointer-events-none">
+                                FIND
+                            </div>
                         </div>
                     </div>
-                    <div className="relative flex justify-center items-center cursor-pointer">
-                        <RoomButton classname="h-auto w-2/3 min-w-[80px] btn-base" />
-                        <div className="absolute text-yellow-custom text-md sm:text-2xl pointer-events-none">
-                            FIND
-                        </div>
-                    </div>
+                    <RoomCancelButton
+                        classname={cx(
+                            "w-[64px] h-auto sm:hidden"
+                        )}
+                        path="/play"
+                    />
                 </div>
             </div>
         </div>
