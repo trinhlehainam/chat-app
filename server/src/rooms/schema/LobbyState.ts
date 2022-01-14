eec6619eb775398d58daf3e4a7f144f1dab338b3
Trinh Le Hai Nam <trinhlehainam1607@gmail.com>
2022-01-14 13:25:58 +0900

    Delete unnecessary files.

--- server/src/rooms/schema/LobbyState.ts
+++
@@ -1,5 +0,0 @@
-import {Schema, type}  from '@colyseus/schema'
-
-export default class LobbyState extends Schema {
-    
-}
--- server/src/rooms/schema/MessageState.ts
+++
@@ -1,5 +0,0 @@
-import {Schema, type} from '@colyseus/schema'
-
-export default class MessageState extends Schema {
-    @type('string') message: string;
-}
