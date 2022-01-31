24a954e062f586b7da173e6fe2d13b0baf9ce257
Nam Trile <trinhlehainam@protonmail.com>
2022-01-31 15:04:40 +0900

    Fixed unsupported prompt style

--- client/src/routes/unsupportedprompt.route.tsx
+++ client/src/routes/unsupportedprompt.route.tsx
@@ -6,7 +6,7 @@ const MemoButton = memo(LobbyButton);
 const UnsupportedPrompt = () => {
     const navigate = useNavigate();
     return (
-        <div className="flex flex-col justify-center items-center min-h-screen gap-6">
+        <div className="flex flex-col justify-center items-center min-h-screen gap-12">
             <h1 className="text-yellow-custom text-7xl">Page is not yet supported !</h1>
             <MemoButton classname="" childClassName="btn-base peer" fillClass="fill" onClick={() => navigate('/')} text="BACK TO HOME" />
         </div>
