971c29cf69f633302d3b4ea0c797ca97e6532770
Nam Trile <trinhlehainam@protonmail.com>
2022-02-07 12:24:53 +0900

    Refactoring server

--- server/src/arena.config.ts
+++ server/src/arena.config.ts
@@ -34,21 +34,16 @@ export default Arena({
         app.use("/colyseus", monitor());
         app.use(express.json());
         app.use(express.urlencoded({ extended: true }));
-        app.use('/colyseus', monitor());
-
-        // NOTE: set up public path for deployment
-        /* const public_path = path.resolve(__dirname, '../build/');
-        app.use(express.static(public_path))
-        app.get('/', (req, res) => {
-            res.sendFile(path.join(public_path, 'index.html'));
-        }); */
 
         /**
          * Bind your custom express routes here:
          */
-        app.get("/", (req, res) => {
-            res.send("It's time to kick ass and chew bubblegum!");
-        });
+        // NOTE: set up public path for deployment
+        /* const public_path = path.resolve(__dirname, '../build/');
+        app.use(express.static(public_path))
+        app.get('/*', (req, res) => {
+            res.sendFile(path.join(public_path, 'index.html'));
+        }); */
     },
 
 
